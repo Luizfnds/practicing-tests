5d688d33490d249b807098119f57db9edcc679b7
Edu <luizeduardofnds@gmail.com>
2022-12-23 18:01:47 -0300

    OrderService Tests

--- src/test/java/com/lefnds/practicingtests/store/OrderServiceTest.java
+++ src/test/java/com/lefnds/practicingtests/store/OrderServiceTest.java
@@ -1,24 +1,48 @@
-//package com.lefnds.practicingtests.store;
-//
-//import com.lefnds.practicingtests.store.order.Order;
-//import com.lefnds.practicingtests.store.order.OrderService;
-//import com.lefnds.practicingtests.store.payment.PaymentService;
-//import com.lefnds.practicingtests.store.user.UserService;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//public class OrderServiceTest {
-//
-//    @Test
-//    public void ThrowsAnExceptionWhenUserIsMinor() {
-//        UserService userService = new UserService();
-//        PaymentService paymentService = new PaymentService();
-//        OrderService orderService = new OrderService(userService, paymentService);
-//
-//        Order order = new Order("1");
-//
-//        IllegalStateException ex = Assertions.assertThrows(IllegalStateException.class, () -> orderService.create(order));
-//
-//        Assertions.assertEquals("Minor user not allowed", ex.getMessage());
-//    }
-//}
+package com.lefnds.practicingtests.store;
+
+import com.lefnds.practicingtests.store.order.Order;
+import com.lefnds.practicingtests.store.order.OrderService;
+import com.lefnds.practicingtests.store.payment.PaymentService;
+import com.lefnds.practicingtests.store.user.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class OrderServiceTest {
+
+    UserService userService = Mockito.mock(UserService.class);
+    PaymentService paymentService = Mockito.mock(PaymentService.class);
+    OrderService orderService = new OrderService(userService, paymentService);
+
+    @Test
+    public void ThrowsAnExceptionWhenUserIsMinor() {
+
+        Order order = new Order("1");
+
+        Mockito.when(userService.isMinor("1")).thenReturn(true);
+        Mockito.doNothing().when(paymentService).pay();
+
+        IllegalStateException ex = Assertions.assertThrows(IllegalStateException.class, () -> orderService.create(order));
+
+        Assertions.assertEquals("Minor user not allowed", ex.getMessage());
+
+        Mockito.verify(userService, Mockito.times(1)).isMinor("1");
+        Mockito.verify(paymentService, Mockito.times(0)).pay();
+
+    }
+
+    @Test
+    public void PayWhenUserIsMajor() {
+
+        Order order = new Order("2");
+
+        Mockito.when(userService.isMinor("2")).thenReturn(false);
+        Mockito.doNothing().when(paymentService).pay();
+
+        orderService.create(order);
+
+        Mockito.verify(userService, Mockito.times(1)).isMinor("2");
+        Mockito.verify(paymentService, Mockito.times(1)).pay();
+
+    }
+}
